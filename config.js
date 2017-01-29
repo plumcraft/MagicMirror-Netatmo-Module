a60d53f3481762656b8be9f1cab13756ea915ec3
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:37:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-netatmo', position: 'bottom_left', config: { clientId: 'ENV_NETATMO_CLIENT_ID', clientSecret: 'ENV_NETATMO_CLIENT_SECRET', refreshToken: 'ENV_NETATMO_TOKEN', refreshInterval: 3, moduleOrder: ["Intérieur", "Salon", "Extérieur"] }, classes: 'default' }
+{ module: 'mmm-netatmo', position: 'bottom_left', config: { clientId: 'ENV_NETATMO_CLIENT_ID', clientSecret: 'ENV_NETATMO_CLIENT_SECRET', refreshToken: 'ENV_NETATMO_TOKEN', refreshInterval: 3, moduleOrder: ["Intérieur", "Salon", "Extérieur"] }, classes: 'default' },
